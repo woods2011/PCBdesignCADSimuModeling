277b8df2e5bcb4e9c66cbeda9251566adaed9e4c
woods2011 <48803341+woods2011@users.noreply.github.com>
2021-06-03 00:07:02 +0300

    Create Test Data

---
+++ TestConcoleApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics.Distributions;
+using PCBdesignCADSimuModeling.Models.Resources;
+using PCBdesignCADSimuModeling.Models.Resources.Algorithms;
+using PCBdesignCADSimuModeling.Models.Resources.Algorithms.PlacingAlgorithms;
+using PCBdesignCADSimuModeling.Models.Resources.Algorithms.WireRoutingAlgorithms;
+using PCBdesignCADSimuModeling.Models.Resources.ResourceRequests;
+using PCBdesignCADSimuModeling.Models.SimuSystem;
+using PCBdesignCADSimuModeling.Models.SimuSystem.SimulationEvents;
+
+namespace TestConcoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+
+            List<Designer> designers = new()
+            {
+                new Designer(Designer.ExperienceEn.Average),
+                new Designer(Designer.ExperienceEn.Average)
+            };
+            CpuThreads cpuThreads = new(16, 2.5);
+            Server server = new(200);
+
+            List<Resource> resourcePool = new();
+            resourcePool.AddRange(designers);
+            resourcePool.Add(cpuThreads);
+
+
+            //
+
+
+            string placingAlgName = "Example";
+            string wireRoutingAlgName = "Example";
+            PcbAlgFactories pcbAlgFactories = new PcbAlgFactories(
+                PlacingAlgProviderFactory.Create(placingAlgName),
+                WireRoutingAlgProviderFactory.Create(wireRoutingAlgName));
+
+
+            //
+
+
+            double intervalDistrMean = new TimeSpan(1, 0, 0, 0).TotalSeconds;
+            double intervalDistrDev = new TimeSpan(6, 0, 0).TotalSeconds;
+            var techIntervalDistr = new Normal(intervalDistrMean, intervalDistrDev);
+
+
+            int elementCountMean = 2000;
+            int elementCountDev = 100;
+            var elementCountDistr = new Normal(elementCountMean, elementCountDev);
+
+            double dimensionUsagePctMean = 0.8;
+            double dimensionUsagePctDev = 0.1;
+            var dimensionUsagePctDistr = new Normal(dimensionUsagePctMean, dimensionUsagePctDev);
+
+            double variousSizePctMean = 0.7;
+            var variousSizePctDistr = new Beta(variousSizePctMean, 1.0 - variousSizePctMean);
+
+
+            var simuEventGenerator = new SimuEventGenerator.Builder()
+                .NewTechInterval(techIntervalDistr)
+                .PcbParams(elementCountDistr, dimensionUsagePctDistr, variousSizePctDistr)
+                .Build();
+
+            //
+
+            TimeSpan finalTime = TimeSpan.FromDays(300);
+
+            PcbDesignCadSimulator simulator =
+                new PcbDesignCadSimulator(simuEventGenerator, resourcePool, pcbAlgFactories);
+            simulator.Simulate(finalTime);
+        }
+    }
+}
